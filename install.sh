3568ae895df222d0daa4ffd7765a0bfab1b30bb0
Alireza <127631174+Alirezad07@users.noreply.github.com>
2023-04-23 07:44:25 +0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,220 @@
+#!/bin/bash
+sed -i 's/#Port 22/Port 22/' /etc/ssh/sshd_config
+po=$(cat /etc/ssh/sshd_config | grep "^Port")
+port=$(echo "$po" | sed "s/Port //g")
+adminuser=$(mysql -N -e "use XPanel; select adminuser from setting where id='1';")
+adminpass=$(mysql -N -e "use XPanel; select adminpassword from setting where id='1';")
+clear
+if [ "$adminuser" != "" ]; then
+adminusername=$adminuser
+adminpassword=$adminpass
+else
+adminusername=admin
+echo -e "\nPlease input Panel admin user."
+printf "Default user name is \e[33m${adminusername}\e[0m, let it blank to use this user name: "
+read usernametmp
+if [[ -n "${usernametmp}" ]]; then
+    adminusername=${usernametmp}
+fi
+adminpassword=123456
+echo -e "\nPlease input Panel admin password."
+printf "Default password is \e[33m${adminpassword}\e[0m, let it blank to use this password : "
+read passwordtmp
+if [[ -n "${passwordtmp}" ]]; then
+    adminpassword=${passwordtmp}
+fi
+fi
+ipv4=$(curl rabin.cf)
+sudo sed -i '/www-data/d' /etc/sudoers &
+wait
+sudo sed -i '/apache/d' /etc/sudoers & 
+wait
+
+if command -v apt-get >/dev/null; then
+apt update -y
+apt remove php8* -y
+sudo apt -y install software-properties-common
+
+sudo add-apt-repository ppa:ondrej/php -y
+
+apt install apache2 php7.4 zip unzip net-tools curl mariadb-server -y
+apt install php7.4-mysql php7.4-xml php7.4-curl -y
+link=$(sudo curl -Ls "https://api.github.com/repos/Alirezad07/X-Panel-SSH-User-Management/releases/latest" | grep '"browser_download_url":' | sed -E 's/.*"([^"]+)".*/\1/')
+sudo wget -O /var/www/html/update.zip $link
+sudo unzip -o /var/www/html/update.zip -d /var/www/html/ &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/sbin/adduser' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/sbin/userdel' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/sed' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/passwd' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/curl' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/wget' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/unzip' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/kill' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/killall' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/lsof' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/sbin/lsof' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/htpasswd' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/sed' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/rm' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/crontab' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/mysqldump' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/sbin/reboot' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/sbin/mysql' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/mysql' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/netstat' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/pgrep' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/sbin/nethogs' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/nethogs' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/local/sbin/nethogs' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/sbin/iptables' | sudo EDITOR='tee -a' visudo &
+wait
+
+sudo service apache2 restart
+chown www-data:www-data /var/www/html/p/* &
+wait
+systemctl restart mariadb &
+wait
+systemctl enable mariadb &
+wait
+sudo phpenmod curl
+PHP_INI=$(php -i | grep /.+/php.ini -oE)
+sed -i 's/extension=intl/;extension=intl/' ${PHP_INI}
+elif command -v yum >/dev/null; then
+yum update -y
+sudo yum -y install https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+sudo yum-config-manager --enable remi-php74 -y
+sudo yum install php php-cli -y
+
+yum install epel-release httpd zip unzip net-tools curl mariadb-server php-mysql php-mysqli php-xml mod_ssl php-curl -y
+systemctl restart httpd
+systemctl restart mariadb &
+wait
+systemctl enable mariadb &
+wait
+link=$(sudo curl -Ls "https://api.github.com/repos/Alirezad07/X-Panel-SSH-User-Management/releases/latest" | grep '"browser_download_url":' | sed -E 's/.*"([^"]+)".*/\1/')
+sudo wget -O /var/www/html/update.zip $link
+sudo unzip -o /var/www/html/update.zip -d /var/www/html/ &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/sbin/adduser' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/sbin/userdel' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/sed' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/passwd' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/curl' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/wget' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/unzip' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/kill' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/killall' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/lsof' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/sbin/lsof' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/htpasswd' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/sed' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/rm' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/crontab' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/mysqldump' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/sbin/reboot' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/sbin/mysql' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/mysql' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/netstat' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/pgrep' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/sbin/nethogs' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/local/sbin/nethogs' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/nethogs' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/sbin/iptables' | sudo EDITOR='tee -a' visudo &
+wait
+po=$(cat /etc/ssh/sshd_config | grep "^Port")
+port=$(echo "$po" | sed "s/Port //g")
+
+systemctl restart httpd
+systemctl enable httpd
+chown apache:apache /var/www/html/p/* &
+wait
+sudo sed -i "s/apache2/httpd/g" /var/www/html/p/setting.php &
+wait
+chmod 644 /etc/ssh/sshd_config &
+wait
+sudo phpenmod curl
+PHP_INI=$(php -i | grep /.+/php.ini -oE)
+sed -i 's/extension=intl/;extension=intl/' ${PHP_INI}
+fi
+bash <(curl -Ls https://raw.githubusercontent.com/Alirezad07/ioncube-loader/main/install.sh --ipv4)
+bash <(curl -Ls https://raw.githubusercontent.com/Alirezad07/Nethogs-Json-main/main/install.sh --ipv4)
+mysql -e "create database XPanel;" &
+wait
+mysql -e "CREATE USER '${adminusername}'@'localhost' IDENTIFIED BY '${adminpassword}';" &
+wait
+mysql -e "GRANT ALL ON *.* TO '${adminusername}'@'localhost';" &
+wait
+sudo sed -i "s/22/$port/g" /var/www/html/p/config.php &
+wait 
+sudo sed -i "s/adminuser/$adminusername/g" /var/www/html/p/config.php &
+wait 
+sudo sed -i "s/adminpass/$adminpassword/g" /var/www/html/p/config.php &
+wait 
+sudo sed -i "s/SERVERUSER/$adminusername/g" /var/www/html/p/killusers.sh &
+wait 
+sudo sed -i "s/SERVERPASSWORD/$adminpassword/g" /var/www/html/p/killusers.sh &
+wait 
+sudo sed -i "s/SERVERIP/$ipv4/g" /var/www/html/p/killusers.sh &
+wait 
+curl -u "$adminusername:$adminpassword" "http://${ipv4}/cp/reinstall.php"
+cp /var/www/html/p/tarikh /var/www/html/p/backup/tarikh
+rm -fr /var/www/html/p/tarikh
+crontab -l | grep -v '/p/expire.php'  | crontab  -
+crontab -l | grep -v '/p/synctraffic.php'  | crontab  -
+(crontab -l ; echo "* * * * * curl  http://${ipv4}/cp/expire.php >/dev/null 2>&1
+* * * * * curl http://${ipv4}/cp/synctraffic.php >/dev/null 2>&1" ) | crontab - &
+wait
+clear
+printf "\nXPanel Link : http://${ipv4}/cp/index.php"
+printf "\nUsername : \e[31m${adminusername}\e[0m "
+printf "\nPassword : \e[31m${adminpassword}\e[0m "
+printf "\nPort : \e[31m${port}\e[0m \n"
