0571e3bda5d9efc9de6afa7b70fbb7995ee33ad4
Alireza <127631174+Alirezad07@users.noreply.github.com>
2023-05-09 02:50:10 +0330

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -120,8 +120,8 @@ done
         wg-quick down wgcf >/dev/null 2>&1
     fi
     
-    ipv4=$(curl rabin.cf)
-    ipv6=$(curl -s6m8 ip.gs)
+    ipv4=$(curl -sm8 ipget.net)
+    ipv6=$(curl -sm8 ipget.net)
     
     echo ""
     yellow "When using port 80 application mode, first point your domain name to your server's public IP address. Otherwise the certificate application will be failed!"
