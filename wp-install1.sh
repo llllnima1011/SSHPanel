ba94e91a3696e6b378cc071d025e1b8859d1080d
Alireza <127631174+Alirezad07@users.noreply.github.com>
2023-05-22 12:56:36 +0330

    Update and rename wp-install.sh to wp-install1.sh

--- wp-install1.sh
+++ wp-install1.sh
@@ -4,13 +4,24 @@ pwd=`dirname $0`
 read -e -p "Target directory: " -i "/var/www/html" dir
 dir=${dir:-"/var/www/html"}
 
-read -e -p "Database: " -i "wordpress" db
+read -e -p "Database AND User: " -i "wordpress" db
 db=${db:-"wordpress"}
+
+read -e -p "Password User: " -i "Xpanel2023" pass
+pass=${pass:-"Xpanel2023"}
 
 if [ ! -d "$dir" ]; then
 	sudo mkdir -p $dir
 fi
 sudo mysql -e "CREATE DATABASE IF NOT EXISTS $db;"
+Q1="CREATE DATABASE $db;"
+Q2="CREATE USER $db@localhost;"
+Q3="SET PASSWORD FOR $db@localhost= PASSWORD('$pass');"
+Q4="GRANT ALL PRIVILEGES on $db.* TO $db@localhost;"
+Q5="FLUSH PRIVILEGES;"
+SQL=${Q1}${Q2}${Q3}${Q4}${Q5}
+
+`mysql -u root -p -e "$SQL"`
 
 cd $dir
 sudo wget http://wordpress.org/latest.zip
