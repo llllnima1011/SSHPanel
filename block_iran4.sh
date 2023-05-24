4c640028d49356d39ddcd3e418031c5395773ce4
Alireza <127631174+Alirezad07@users.noreply.github.com>
2023-05-24 07:35:27 +0330

    Update block_iran4.sh

--- block_iran4.sh
+++ block_iran4.sh
@@ -8,12 +8,11 @@ wait
 sudo apt-get install libtext-csv-xs-perl libmoosex-types-netaddr-ip-perl &
 wait
 sudo mkdir /usr/share/xt_geoip
-cd /usr/share/xt_geoip
 wait
 MON=$(date +"%m")
 YR=$(date +"%Y")
 
-wget https://download.db-ip.com/free/dbip-country-lite-${YR}-${MON}.csv.gz -O /usr/share/xt_geoip/dbip-country-lite.csv.gz
+wget https://download.db-ip.com/free/dbip-country-lite-2023-05.csv.gz -O /usr/share/xt_geoip/dbip-country-lite.csv.gz
 gunzip /usr/share/xt_geoip/dbip-country-lite.csv.gz
 /usr/lib/xtables-addons/xt_geoip_build -D /usr/share/xt_geoip/ -S /usr/share/xt_geoip/
 rm /usr/share/xt_geoip/dbip-country-lite.csv
