4a1ce127fdd48324231a2c6e7c6b168738f4035b
Alireza <127631174+Alirezad07@users.noreply.github.com>
2023-06-07 18:57:49 +0330

    Update and rename i36.sh to i37.sh

--- i37.sh
+++ i37.sh
@@ -23,17 +23,17 @@ dropb_port=$(mysql -N -e "use XPanel; select dropb_port from setting where id='1
 dropb_tls_port=$(mysql -N -e "use XPanel; select dropb_tls_port from setting where id='1';")
 ssh_tls_port=$(mysql -N -e "use XPanel; select ssh_tls_port from setting where id='1';")
 clear
-if [ "$dropb_port" != "" ]; then
+if [ "$dropb_port" != "" && "$dropb_port" != "NULL" ]; then
 dropbear_port=$dropb_port
 else
 dropbear_port=222
 fi
-if [ "$dropb_tls_port" != "" ]; then
+if [ "$dropb_tls_port" != "" && "$dropb_tls_port" != "NULL" ]; then
 dropbear_tls_port=$dropb_tls_port
 else
 dropbear_tls_port=2083
 fi
-if [ "$ssh_tls_port" != "" ]; then
+if [ "$ssh_tls_port" != "" && "$ssh_tls_port" != "NULL" ]; then
 sshtls_port=$ssh_tls_port
 else
 sshtls_port=444
